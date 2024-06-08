26ebc109adb171dc526f8fcf34d606039b4b0b55
DamiaoCanndido <tower_gamer@hotmail.com>
2024-06-08 14:03:36 -0300

    join to normal pool

--- src/app/(main)/my-pools/[poolId]/[gameId]/page.tsx
+++ src/app/(main)/my-pools/[poolId]/[gameId]/page.tsx
@@ -6,6 +6,8 @@ import { api } from '@/lib/axios';
 import { AuthContextGlobal } from '@/providers/auth';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
 
 interface Props {
   params: { poolId: string; gameId: string };
@@ -15,6 +17,7 @@ export default function Page({ params }: Props) {
   const { token } = AuthContextGlobal();
   const [game, setGame] = useState<Game>();
   const [guess, setGuess] = useState<Guess | null>(null);
+  const [guesses, setGuesses] = useState<Guess[]>([]);
 
   useEffect(() => {
     const getGame = async () => {
@@ -37,15 +40,44 @@ export default function Page({ params }: Props) {
         }
       );
       setGuess(guessResult.data.guess);
-      console.log(`${JSON.stringify(guessResult.data.guess)}`);
+    };
+
+    const getGuesses = async () => {
+      const guessesResult = await api.get(
+        `/pool/${params.poolId}/game/${params.gameId}/guess`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setGuesses(guessesResult.data.guesses);
     };
 
     getGame();
     getMyGuess();
+    getGuesses();
   }, []);
   return (
     <main className="flex flex-col space-y-2 items-center w-[600px] h-full p-2 border rounded-lg mx-auto max-lg:ml-auto border-green-600 mt-[72px]">
       <GuessCard game={game!} guess={guess} poolId={params.poolId} />
+      {guesses.length === 0 ? (
+        <p className="text-center">
+          Ainda não é possível vê palpites de outros jogadores espere até o jogo
+          começar.
+        </p>
+      ) : (
+        guesses.map((e) => {
+          return (
+            <div className="flex items-center w-full space-y-2 shadow-md h-12">
+              <p>
+                {`${e.player.user.name} colocou ${e.homeScore}:${e.awayScore} às
+                ${dayjs(e.createdAt).format('DD/MM/YY hh:mm A')}`}
+              </p>
+            </div>
+          );
+        })
+      )}
     </main>
   );
 }
--- src/components/owner/pool-detailed.tsx
+++ src/components/owner/pool-detailed.tsx
@@ -5,6 +5,11 @@ import utc from 'dayjs/plugin/utc';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { useRouter, usePathname } from 'next/navigation';
 import { useEffect } from 'react';
+import { Button } from '../ui/button';
+import { api } from '@/lib/axios';
+import { AuthContextGlobal } from '@/providers/auth';
+import { useToast } from '@/components/ui/use-toast';
+import { ToastAction } from '@/components/ui/toast';
 
 dayjs.extend(relativeTime);
 dayjs.locale('pt-br');
@@ -13,13 +18,46 @@ dayjs.extend(utc);
 export default function PoolDetailed(pool: Pool) {
   const router = useRouter();
   const path = usePathname();
+  const { toast } = useToast();
 
-  useEffect(() => {
-    console.log(pool.code);
-  }, [pool]);
+  const { token } = AuthContextGlobal();
+
+  async function joinToNormal() {
+    try {
+      await api.post(
+        `/pools/${pool.id}/normal`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast({
+        title: 'Entrou no bolão com sucesso.',
+        style: {
+          backgroundColor: 'green',
+          borderColor: 'white',
+        },
+        variant: 'default',
+        action: (
+          <ToastAction color="white" altText="fechar">
+            Fechar
+          </ToastAction>
+        ),
+      });
+      router.replace('/my-pools');
+    } catch (error) {
+      toast({
+        title: 'Erro não é possível entrar nesse bolão.',
+        variant: 'destructive',
+        action: <ToastAction altText="fechar">fechar</ToastAction>,
+      });
+    }
+  }
 
   return (
-    <div className="flex flex-col w-full h-auto bg-green-200 rounded-lg cursor-pointer hover:bg-green-300">
+    <div className="flex flex-col w-full h-auto bg-green-200 rounded-lg">
       <div className="flex h-14 justify-between mx-1">
         <p>{pool.name}</p>
         <p>{pool.league ? pool.league : 'Sem liga'}</p>
@@ -32,6 +70,12 @@ export default function PoolDetailed(pool: Pool) {
       </div>
       <div className="flex h-14 justify-between mx-1">
         <p>{`${pool.gamesClosed}/${pool.nGames}`}</p>
+        <Button
+          onClick={joinToNormal}
+          className="w-40 bg-green-600 hover:bg-green-400"
+        >
+          Entrar
+        </Button>
       </div>
     </div>
   );
